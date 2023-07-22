4f18a73c2f945225fa662d1023e98cac9f7f424b
Coder0712 <84794107+Coder0712@users.noreply.github.com>
2023-07-22 10:14:23 +0200

    created a user model

---
+++ UserManagement/Model/User.cs
@@ -0,0 +1,33 @@
+﻿namespace UserManagement.Model
+{
+    /// <summary>
+    /// Defines a model for the user
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// Gest or sets the first name.
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Gest or sets the last name.
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Gest or sets the Email address.
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Gest or sets the enabled state.
+        /// </summary>
+        public bool Enabled { get; set; }
+
+        /// <summary>
+        /// Gest or sets the username.
+        /// </summary>
+        public string Username { get; set; }
+    }
+}
